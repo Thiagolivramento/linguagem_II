434d1539fdd37a6dfe78ff3bbebde72308c25f3b
Leonardo Tasca <ltasca93@gmail.com>
2020-08-12 20:03:55 -0300

    atualizacao

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,10 @@
 '''Escreva uma classe que contém um método que calcule se a pessoa é maior de 18 anos ou não. 
 Receba os dados pela console e chame este método.'''
 
-class 
+class MaiorIdade:
+    
+    def calcular_idade(self, idade):
+        if idade >= 18:
+            print('Maior de 18')
+        else:
+            print('Menor de 18')
