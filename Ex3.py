f9e88a7661ec1f1881ea3b95753194de0f89ed63
Leonardo Tasca <ltasca93@gmail.com>
2020-08-12 19:48:03 -0300

    12/08/2020

---
+++ Ex3.py
@@ -0,0 +1,3 @@
+'''Crie uma classe calculadora com as quatro operações básicas (soma, subtração,
+multiplicação e divisão). O usuário deve informar dois números e o programa deve
+fazer as quatro operações.'''
---
+++ Ex4.py
@@ -0,0 +1,2 @@
+'''Faça um programa que receba um valor que é o valor pago, um segundo valor que
+é o preço do produto e retorne o troco a ser dado.'''
