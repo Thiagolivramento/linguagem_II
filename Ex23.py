9e8016e9efd950b3c1a0a4151f3bf16fd394641c
Thiagolivramento <tlivramento.adapta@gmail.com>
2020-09-02 21:25:53 -0300

    continuação das atividades

---
+++ Ex23.py
@@ -0,0 +1,22 @@
+#criando uma variavel lista
+lista = [] 
+
+
+#criando uma classe
+class Utils:
+#função adicionar item na lista
+    def add_lista(self, item):
+        lista.append(item)
+        print(lista)
+        print('a) Maior Elemento', max(lista))
+        print('b) Soma dos Elementos', sum(lista))
+
+
+util = Utils()
+
+
+while True:
+    numero = int(input("Digite um numero: ou -0 para sair: "))
+    util.add_lista(numero)
+    if numero == -0:
+        break
